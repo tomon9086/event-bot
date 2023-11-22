c717f9751e3cad7155c9a9f4a4a191c4b74b5bd5
tomon9086 <tomon9086@gmail.com>
2023-11-22 16:24:53 +0900

    set cron

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,13 @@
 import { https, pubsub } from 'firebase-functions'
-import { DateTime } from 'luxon'
 import { postNewEvents } from './app/event'
 
 export const ping = https.onRequest((_, res) => {
-  postNewEvents()
-
   res.send('pong')
 })
 
 export const everyday = pubsub
-  .schedule('0 9 * * *')
+  .schedule('0 9-21 * * *')
   .timeZone('Asia/Tokyo')
-  .onRun((_context) => {
-    const now = DateTime.now().setZone('Asia/Tokyo')
-
-    console.log(now)
+  .onRun(async (_context) => {
+    await postNewEvents()
   })
