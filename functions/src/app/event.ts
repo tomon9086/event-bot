97c6343942d8882878c0bc1606940f1c55f347c2
tomon9086 <tomon9086@gmail.com>
2023-11-23 20:20:27 +0900

    insert log

--- functions/src/app/event.ts
+++ functions/src/app/event.ts
@@ -24,6 +24,22 @@ export const postNewEvents = async () => {
     (event) => !messagesFromMe?.find((m) => m.content.includes(event.event_url))
   )
 
+  console.log(
+    events
+      .map((event) => {
+        const isNew = !messagesFromMe?.find((m) =>
+          m.content.includes(event.event_url)
+        )
+
+        return `"${event.event_id}" is${isNew ? '' : ' NOT'} new event: 「${
+          event.title
+        }」`
+      })
+      .join('\n') +
+      '\n\n' +
+      `new events count: ${newEvents.length}`
+  )
+
   await Promise.all(
     newEvents
       .map((event) => formatEventMessage(event))
